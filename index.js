acb403703f66441c12cbe2de13ffb7f129ba0ae7
Vigen Hovhannisyan <vhovhannisyan@griddynamics.com>
2025-02-10 08:48:21 +0400

    clean up project

--- index.js
+++ index.js
@@ -7,23 +7,18 @@ require('dotenv').config();
 
 const app = express();
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Serve static files
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Home route
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-// Use routes
 app.use('/api', userRoutes);
 app.use('/api', exerciseRoutes);
 
-// Start server
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port);
 });
